610a0d6b45b31a07c006240b4c0e40a30ff9956a
Shahrizan <shahrizan.learner@gmail.com>
2023-12-26 23:42:26 +0700

    Add Listener class for message listening

---
+++ src/listener.js
@@ -0,0 +1,23 @@
+class Listener {
+  constructor(mailSender, playlistService) {
+    this._mailSender = mailSender;
+    this._playlistService = playlistService;
+  }
+
+  async listen(message) {
+    try {
+      const { playlistId, targetEmail } = JSON.parse(
+        message.content.toString()
+      );
+
+      const playlist = await this._playlistService.getPlaylists(playlistId);
+      const result = await this._mailSender.sendEmail(
+        targetEmail,
+        JSON.stringify(playlist)
+      );
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
