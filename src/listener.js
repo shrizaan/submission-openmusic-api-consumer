c9cd79562e537c3ad321faf5fc29e47707a8f7fb
Shahrizan <shahrizan.learner@gmail.com>
2023-12-26 23:51:23 +0700

    Add ESLint configuration and update dependencies

--- src/listener.js
+++ src/listener.js
@@ -9,13 +9,13 @@ class Listener {
   async listen(message) {
     try {
       const { playlistId, targetEmail } = JSON.parse(
-        message.content.toString()
+        message.content.toString(),
       );
 
       const playlist = await this._playlistService.getPlaylists(playlistId);
       const result = await this._mailSender.sendEmail(
         targetEmail,
-        JSON.stringify(playlist)
+        JSON.stringify(playlist),
       );
       console.log(result);
     } catch (error) {
